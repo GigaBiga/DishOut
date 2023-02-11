3145c52e52ee9f573eb849553cb8e99f68d9f1e5
Rui Reis <ruimartinseis@gmail.com>
2023-02-11 19:56:02 +0000

    Refined Table status and implemented stakeholder

--- DishOut/static/waiter/js/tableStatusJS.js
+++ DishOut/static/waiter/js/tableStatusJS.js
@@ -27,33 +27,6 @@ window.addEventListener('load', restoreScrollPosition);
 
 // Timer function
 
-// Checks if the number is minus and if it is it changes it so that it is a posative
-function is_minus(number){
-  //  is the time negative
-  if (number < 0){
-    // If it is add it to 60 which will give you the difference which will be the correct time
-    number = 60 + number; 
-    // Now return the number
-    return number;
-  } else {
-    // If it is not negative just return the number
-    return number;
-  };
-}
-
-// Takes in the start time and the table number for which it is for
-// function updateTimer(startTime,tableNumber) {
-//   // Gets the current date and saves it to currentTime
-//   let currentTime = new Date();
-//   // Splits the start time into the hours, minutes and seconds 
-//   startTime = startTime.split(':')
-//   // minuses the current hours, minutes and seconds from the start time ones then also adds a colon inbetween them
-//   // It also uses the is_minus function I made
-//   let difference = (currentTime.getHours()-startTime[0]) + ':' + is_minus(currentTime.getMinutes()-startTime[1]) + ':' + is_minus(currentTime.getSeconds()-startTime[2]);
-//   // changes the contents of the timer element for that table with the difference
-//   document.getElementById(`timer-${tableNumber}`).innerHTML = difference;
-// }
-
 // Timer data retriver and updater
 function get_times(){
   // Fetches the table start times and table numbers from the API
@@ -69,30 +42,52 @@ function get_times(){
           const tableNumber = data[i].Table_Number;
           const timerStatus = data[i].Timer_Status;
           // Sends the start time and the table number to calculate the timer for to the updateTimer function
-          NewUpdateTimer(timerStatus,tableNumber)
+          updateTimer(timerStatus,tableNumber)
         }
     });
   };
 
+// Updates timer using the difference in seconds
+function updateTimer (startTime,tableNumber){
+  // Gets the current time
+  let currentTime = new Date();
+  // Converts the current time into seconds
+  currentTime = (currentTime.getHours() * 3600) + (currentTime.getMinutes() * 60) + currentTime.getSeconds();
+  // Splits the start time of the table into an array holding the hour, minutes, and seconds
+  startTime = startTime.split(':');
+  // Finds the start time of the table in seconds
+  startTime = ((startTime[0] * 3600)+(startTime[1] * 60)+(startTime[2]*1));
+  // Variable that holds the difference in seconds
+  var differenceSEC = currentTime - startTime;
+  // Variable that holds the difference in hours converted from seconds. It truncates and makes value always positive
+  var differenceHours = Math.abs(Math.trunc((differenceSEC/3600)));
+  // Subtracts the differenceHours from the diffence in seconds then it can divide by 60 to find the minutes
+  var differenceMinutes = Math.abs(Math.trunc((differenceSEC-(differenceHours*3600))/60));
+  // Subtracts all the previous ones which will just equal the second
+  var differenceSeconds = Math.abs(Math.trunc((differenceSEC-(differenceHours*3600))-differenceMinutes*60));
+  // Then creates a variable which combines all the difference in times and connects them using colons. 
+  // Uses the is_double function to find if they are double digits
+  let difference = is_double(differenceHours) + ':' + is_double(differenceMinutes) + ':' + is_double(differenceSeconds);
+  // Changes the table status timer to equal the correct time
+  document.getElementById(`timer-${tableNumber}`).innerHTML = difference;
+}
 
+// Checks if a time is not double digits and makes it display as double digits
+function is_double(time){
+  // if time is less than 10 it must be singular digits
+  if(time < 10){
+    // converts time into a string and adds a string 0 infront of it
+    time = "0" + String(time);
+    // Returns converted time
+    return time;
+  } // If the time is larger than 10 or 10 it doesn't need to be converted so it is returned 
+  else{ return time};
+}
 
 
 // Refresh the page every 5 seconds
-setInterval(refreshPage, 20000);
+setInterval(refreshPage, 5000);
 
 // refreshes the timer every second and runs it instatly whenever the page is refreshed
 get_times();
 setInterval(get_times, 1000)
-
-function NewUpdateTimer (startTime,tableNumber){
-  let currentTime = new Date();
-  currentTime = (currentTime.getHours() * 3600) + (currentTime.getMinutes() * 60) + currentTime.getSeconds();
-  startTime = startTime.split(':');
-  startTime = ((startTime[0] * 3600)+(startTime[1] * 60)+(startTime[2]*1));
-  var differenceSEC = currentTime - startTime;
-  var differenceHours = Math.abs(Math.trunc((differenceSEC/3600)));
-  var differenceMinutes = Math.abs(Math.trunc((differenceSEC-(differenceHours*3600))/60));
-  var differenceSeconds = Math.abs(Math.trunc((differenceSEC-(differenceHours*3600))-differenceMinutes*60));
-  let difference = (differenceHours) + ':' + (differenceMinutes) + ':' + (differenceSeconds);
-  document.getElementById(`timer-${tableNumber}`).innerHTML = difference;
-}
